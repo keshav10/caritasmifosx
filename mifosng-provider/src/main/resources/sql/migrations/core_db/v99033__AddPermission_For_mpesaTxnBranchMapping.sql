2f29c99b67b6cc74fa416a3e2f74ebda784ed8a8
awaleram <ram.awale@confuxtechnologies.com>
2016-04-21 16:53:58 +0530

    added migration script for inserting records in m_permission for the mpesa_txn_mapping table

---
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/v99033__AddPermission_For_mpesaTxnBranchMapping.sql
@@ -0,0 +1,28 @@
+
+
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES
+ ('datatable', 'CREATE_MpesaTxnBranchMapping', 'MpesaTxnBranchMapping', 'CREATE', 1);
+
+ INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+ ('datatable', 'CREATE_MpesaTxnBranchMapping_CHECKER', 'MpesaTxnBranchMapping', 'CREATE', 0);
+ 
+ 
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+('datatable', 'READ_MpesaTxnBranchMapping', 'MpesaTxnBranchMapping', 'READ', 0);
+
+
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+('datatable', 'UPDATE_MpesaTxnBranchMapping', 'MpesaTxnBranchMapping', 'UPDATE', 1);
+
+
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+('datatable', 'UPDATE_MpesaTxnBranchMapping_CHECKER', 'MpesaTxnBranchMapping', 'UPDATE', 0);
+
+
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+('datatable', 'DELETE_MpesaTxnBranchMapping', 'MpesaTxnBranchMapping', 'DELETE', 1);
+
+
+
+INSERT INTO `m_permission` (`grouping`, `code`, `entity_name`, `action_name`, `can_maker_checker`) VALUES 
+('datatable', 'DELETE_MpesaTxnBranchMapping_CHECKER', 'MpesaTxnBranchMapping', 'DELETE', 0);
